31786e2bb5d86e72cd059864a70745d767059018
Javier David Banda V. <52288978+jdavidbv@users.noreply.github.com>
2025-12-03 17:39:23 -0600

    Initialize separate_data.py for data analysis
    Add initial structure for data processing in separate_data.py

--- Codigo/separate_data.py
+++ Codigo/separate_data.py
@@ -1,1 +1,27 @@
+import pandas as pd
 
+"""
+Read xlsx
+"""
+path_ = ""
+excel_sheets = ´pd.xlsx_read(path+"Tareas")
+
+"""
+Crear DF por cada hoja si lo veo necesario o solo invocarlo
+"""
+pass
+
+"""
+Analizar graficamente o de alguna forma que datos trae cada elemento.
+Aquí voy a determinar que es dato y que es basura
+"""
+pass
+
+"""
+Consolidar por columnas en un nuevo DataFrame, quizas necesite agregar columna de fechas
+"""
+pass
+
+"""
+Nuevo analisis grafico
+"""
